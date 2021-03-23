dd8b86bdcf03461e7dae86d0b31906713a16c939
Sourav Goswami <souravgoswami@protonmail.com>
2021-03-23 16:56:08 +0530

    More changes to cookie notice

--- app/javascript/packs/cookie_notice.js
+++ app/javascript/packs/cookie_notice.js
@@ -1,18 +1,37 @@
 const cookieContainer = document.getElementById('cookie-container')
-const closeCookieBtn = document.getElementById('close-cookie')
 
-if (localStorage.cookie == 1 && cookieContainer)
-  document.body.removeChild(cookieContainer)
+if (localStorage.cookie != 1 && cookieContainer) {
+  const closeCookieBtn = document.getElementById('close-cookie')
+  const pixels = Array.from(document.getElementsByClassName('pixels'))
 
-if (closeCookieBtn) {
-  closeCookieBtn.onclick = function() {
-    cookieContainer.style.animation = 'fade-out 1.5s ease forwards'
+  // Create small pixels in the cookie notice
+  const colours = ['4AEE9C', '1DE37F', '33EEBB', '31FAAE']
+  for(let _i = 0 ; _i < 5 ; ++_i) {
+    let node = document.createElement('div')
+    node.className = 'pixels'
 
-    setTimeout(() => {
-      if (cookieContainer)
-        document.body.removeChild(cookieContainer)
+    let style = node.style
+    let randomColour = colours[Math.floor(Math.random() * colours.length)]
 
-      localStorage.cookie = 1
-    }, 2000)
+    style.backgroundColor = `#${randomColour}`
+    style.padding = `${Math.floor(Math.random() * 6) + 2}px`
+    style.top = `${Math.random() * 100}%`
+    style.left = `${Math.random() * 100}%`
+
+    cookieContainer.appendChild(node)
   }
+
+  if (closeCookieBtn) {
+    closeCookieBtn.onclick = function() {
+      cookieContainer.style.animation = 'fade-out 1.5s ease forwards'
+
+      setTimeout(() => {
+        if (cookieContainer) document.body.removeChild(cookieContainer)
+        localStorage.cookie = 1
+      }, 3000)
+    }
+  }
+} else if (cookieContainer) {
+  // Remove the cookieContainer if it's previously dismissed by the user
+  document.body.removeChild(cookieContainer)
 }
